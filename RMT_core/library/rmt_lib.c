5342b587214b37c276bdd6f6113b8323efedf25f
ChenYing Kuo <evshary@gmail.com>
2021-03-12 18:27:18 +0800

    Add the RMT_library.

---
+++ RMT_core/library/rmt_lib.c
@@ -0,0 +1,5 @@
+#include "rmt_lib.h"
+
+char* rmt_lib_version(void) {
+    return "0.9.0";
+}
---
+++ RMT_core/library/rmt_lib.h
@@ -0,0 +1,6 @@
+#ifndef _RMT_LIB_
+#define _RMT_LIB_
+
+char* rmt_lib_version(void);
+
+#endif /*_RMT_LIB_*/
