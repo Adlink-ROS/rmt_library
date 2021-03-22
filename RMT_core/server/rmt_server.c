2393336f5a1220525d6965b9868f63863ccaba05
ChenYing Kuo <evshary@gmail.com>
2021-03-22 10:12:21 +0800

    Add API for get/set info.

--- RMT_core/server/rmt_server.c
+++ RMT_core/server/rmt_server.c
@@ -2,18 +2,32 @@
 #include "config.h"
 #include "device_info.h"
 
-int rmt_server_init(void) {
+int rmt_server_init(void)
+{
     return device_info_subscriber_init();
 }
 
-int rmt_server_list_device(void) {
+int rmt_server_list_device(void)
+{
     return list_device_info();
 }
 
-int rmt_server_deinit(void) {
+int rmt_server_get_info(void)
+{
+    return 0;
+}
+
+int rmt_server_set_info(void)
+{
+    return 0;
+}
+
+int rmt_server_deinit(void)
+{
     return device_info_subscriber_deinit();
 }
 
-char* rmt_server_version(void) {
+char* rmt_server_version(void)
+{
     return PROJECT_VERSION;
 }
--- RMT_core/server/rmt_server.h
+++ RMT_core/server/rmt_server.h
@@ -3,6 +3,8 @@
 
 int rmt_server_init(void);
 int rmt_server_list_device(void);
+int rmt_server_get_info(void);
+int rmt_server_set_info(void);
 int rmt_server_deinit(void);
 char* rmt_server_version(void);
 
