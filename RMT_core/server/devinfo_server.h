b1c0da4dfe609734f9221e0ad2efe13cafa6ee05
ChenYing Kuo <evshary@gmail.com>
2021-03-30 14:03:11 +0800

    Update the definition of header files.

--- RMT_core/server/devinfo_server.h
+++ RMT_core/server/devinfo_server.h
@@ -1,5 +1,5 @@
-#ifndef _DEVICE_INFO_
-#define _DEVICE_INFO_
+#ifndef _DEVICEINFO_SERVER_H_
+#define _DEVICEINFO_SERVER_H_
 
 int devinfo_server_config(char *interface);
 int devinfo_server_init(void);
@@ -7,4 +7,4 @@ int devinfo_server_create_list(device_info **dev, uint32_t *num);
 int devinfo_server_free_list(device_info **dev);
 int devinfo_server_deinit(void);
 
-#endif /*_DEVICE_INFO_*/
+#endif /*_DEVICEINFO_SERVER_H_*/
