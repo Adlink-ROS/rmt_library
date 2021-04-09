06a640ad3e6bf0dd1345326a7412571f9b43997e
Ting Chang <ting.chang@adlinktech.com>
2021-04-09 17:53:37 +0800

    Refactoring python_example.py in swig

--- RMT_core/swig/python_example.py
+++ RMT_core/swig/python_example.py
@@ -2,6 +2,20 @@
 import rmt_py_wrapper
 import json
 
+def config(dev_list, dev_num):
+    id_list = rmt_py_wrapper.ulong_array(dev_num)
+    for i in range(0, dev_num):
+        id_list[i] = dev_list[i].deviceID
+    info_num_ptr = rmt_py_wrapper.new_intptr()
+    info_list = rmt_py_wrapper.data_info_list.frompointer(rmt_py_wrapper.rmt_server_get_info(id_list, dev_num, "cpu", info_num_ptr))
+    info_num = rmt_py_wrapper.intptr_value(info_num_ptr)
+    rmt_py_wrapper.delete_intptr(info_num_ptr) # release info_num_ptr
+    
+    print("=== get config result ===")
+    for i in range(0, info_num):
+        print("ID %d" % info_list[i].deviceID)
+        print("value list: %s" % info_list[i].value_list)
+
 def search():
     rmt_py_wrapper.rmt_server_init()
 
@@ -11,7 +25,8 @@ def search():
     rmt_py_wrapper.delete_intptr(num_ptr) # release num_ptr
 
     # Put data in JSON format    
-    data=[{"Total": num}]
+    data = {"total": num, "items": []}
+    items = []
     for i in range(0, num):
         item = {
             "ID": dev_list[i].deviceID,
@@ -21,25 +36,18 @@ def search():
             "MAC": dev_list[i].mac,
             "RMT_VERSION": dev_list[i].rmt_version
         }
-        data.append(item)
+        items.append(item)
+
+    print("=== search result ===")
+    data["items"] = items
     result = json.dumps(data, indent=4)
     print(result)
-
-    # put search ID into array
-    id_list = rmt_py_wrapper.ulong_array(num)
-    for i in range(0, num):
-        id_list[i] = dev_list[i].deviceID
-    info_num_ptr = rmt_py_wrapper.new_intptr()
-    info_list = rmt_py_wrapper.data_info_list.frompointer(rmt_py_wrapper.rmt_server_get_info(id_list, num, "cpu", info_num_ptr))
-    info_num = rmt_py_wrapper.intptr_value(info_num_ptr)
-    rmt_py_wrapper.delete_intptr(info_num_ptr) # release info_num_ptr
-    for i in range(0, info_num):
-        print("ID %d" % info_list[i].deviceID)
-        print("value list: %s" % info_list[i].value_list)
+    return dev_list, num
 
 def main():
     print("RMT_VERSION=%s" % rmt_py_wrapper.rmt_server_version())
+    dev_list, num = search()
+    config(dev_list, num)
 
 if __name__ == "__main__":
     main()
-    search()
