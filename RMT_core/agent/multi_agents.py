d3d6d71dc540e09caafe8f048a1d6cfcbc6312cb
Ting Chang <ting.chang@adlinktech.com>
2021-04-23 09:49:14 +0800

    Update multi_agents.py to support network interface

--- RMT_core/agent/multi_agents.py
+++ RMT_core/agent/multi_agents.py
@@ -11,14 +11,15 @@ os.environ['LD_LIBRARY_PATH'] = os.getcwd()
 
 def usage():
     print("Usage:")
+    print("\t-i <net_interface>")
     print("\t-n <device_num>")
     print("\t-s <start_id>")
     print("Example:")
-    print("\tpython multi_agents.py -n 5 -s 5566")    
+    print("\tpython multi_agents.py -i wlp2s0 -n 5 -s 5566")    
 
-def worker(id):
+def worker(inf, id):
     """thread worker function"""
-    subprocess.run(["./agent_example", "--id", str(id)])
+    subprocess.run(["./agent_example", "--net", inf, "--id", str(id)])
     return
 
 def sig_handler(arg1, arg2):
@@ -28,18 +29,21 @@ def sig_handler(arg1, arg2):
 
 def main(args):
     try:
-        opts, args = getopt.getopt(args, "hn:s:", ["help", "num=", "start_id="])
+        opts, args = getopt.getopt(args, "hi:n:s:", ["help", "interface=", "num=", "start_id="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         usage()
         sys.exit(2)
+    interface = ""
     number = 1
     start_id = 1
     for o, a in opts:
         if o in ("-h", "--help"):
             usage()
             sys.exit()
+        elif o in ("-i", "--interface"):
+            interface = a
         elif o in ("-n", "--number"):
             number = int(a)
         elif o in ("-s", "--start_id"):
@@ -47,11 +51,11 @@ def main(args):
         else:
             assert False, "unhandled option"    
 
-    print("number=%d, start_id=%d" % (number, start_id))
+    print("interface=%s, number=%d, start_id=%d" % (interface, number, start_id))
 
     jobs = []
     for i in range(number):
-        p = multiprocessing.Process(target=worker, args=((start_id+i),))
+        p = multiprocessing.Process(target=worker, args=(interface, (start_id+i),))
         jobs.append(p)
         p.start()
 
