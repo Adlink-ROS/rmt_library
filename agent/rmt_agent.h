4e56c8025d8204e668ba88f0f6f9513020d2c032
ChenYing Kuo <evshary@gmail.com>
2021-04-26 10:54:56 +0800

    Support C++ in agent side.

--- agent/rmt_agent.h
+++ agent/rmt_agent.h
@@ -1,10 +1,14 @@
 #ifndef _RMT_AGENT_
 #define _RMT_AGENT_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef int (*INFO_FUNC)(char *);
 
 typedef struct datainfo_func {
-    char *key;
+    const char *key;
     INFO_FUNC get_func;
     INFO_FUNC set_func;
 } datainfo_func;
@@ -14,5 +18,9 @@ int rmt_agent_init(datainfo_func *func_maps);
 int rmt_agent_running(void);
 int rmt_agent_deinit(void);
 char* rmt_agent_version(void);
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif 
 
 #endif /*_RMT_AGENT_*/
