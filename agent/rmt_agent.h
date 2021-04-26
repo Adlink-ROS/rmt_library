5deaccc8558c368ca444d0d796d0d31edf9a74d3
ChenYing Kuo <evshary@gmail.com>
2021-04-26 11:24:53 +0800

    Remove agent example (#14)
    * Don't build agent_example.
    
    * Remove build mraa.
    
    * Seperate the agent example project.
    
    * Support C++ in agent side.
    
    * Use correct branch for agent example.
    
    * Ignore the extern C issue of uncrustify.

--- agent/rmt_agent.h
+++ agent/rmt_agent.h
@@ -1,10 +1,16 @@
 #ifndef _RMT_AGENT_
 #define _RMT_AGENT_
 
+/* *INDENT-OFF* */
+#ifdef __cplusplus
+extern "C" {
+#endif
+/* *INDENT-ON* */
+
 typedef int (*INFO_FUNC)(char *);
 
 typedef struct datainfo_func {
-    char *key;
+    const char *key;
     INFO_FUNC get_func;
     INFO_FUNC set_func;
 } datainfo_func;
@@ -14,5 +20,11 @@ int rmt_agent_init(datainfo_func *func_maps);
 int rmt_agent_running(void);
 int rmt_agent_deinit(void);
 char* rmt_agent_version(void);
+
+/* *INDENT-OFF* */
+#ifdef __cplusplus
+} /* extern "C" */
+#endif 
+/* *INDENT-ON* */
 
 #endif /*_RMT_AGENT_*/
