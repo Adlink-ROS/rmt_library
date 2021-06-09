3885877ff7135aeb208e9d337b8f3c97c1824012
ChenYing Kuo <evshary@gmail.com>
2021-06-09 09:54:07 +0800

    Modify the type of internal id.

--- server/devinfo_server.h
+++ server/devinfo_server.h
@@ -1,9 +1,11 @@
 #ifndef _DEVICEINFO_SERVER_H_
 #define _DEVICEINFO_SERVER_H_
 
+#include <stdint.h>
+
 extern transfer_result empty_result;
 
-int devinfo_server_del_device_callback(long internal_id);
+int devinfo_server_del_device_callback(uint64_t internal_id);
 int devinfo_server_update(struct dds_transport *transport);
 int devinfo_server_create_list(struct dds_transport *transport, device_info **dev, uint32_t *num);
 int devinfo_server_free_list(device_info *dev);
