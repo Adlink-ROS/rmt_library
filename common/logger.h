2373bf0202954eb99cf7dacfbbf7de589fde02ed
ChenYing Kuo <evshary@gmail.com>
2021-06-25 18:07:34 +0800

    Use fprintf instead of printf.

--- common/logger.h
+++ common/logger.h
@@ -10,8 +10,8 @@
 #if RMT_LOG_ENABLE
  # define RMT_LOG(...)             \
           do {                     \
-              printf("RMT_LOG:");  \
-              printf(__VA_ARGS__); \
+              fprintf(stderr, "RMT_LOG:");  \
+              fprintf(stderr, __VA_ARGS__); \
           } while(0);
 #else
  # define RMT_LOG
@@ -20,8 +20,8 @@
 #if RMT_WARN_ENABLE
  # define RMT_WARN(...)            \
           do {                     \
-              printf("RMT_WARN:"); \
-              printf(__VA_ARGS__); \
+              fprintf(stderr, "RMT_WARN:"); \
+              fprintf(stderr, __VA_ARGS__); \
           } while(0);
 #else
  # define RMT_WARN
@@ -30,8 +30,8 @@
 #if RMT_ERROR_ENABLE
  # define RMT_ERROR(...)           \
           do {                     \
-             printf("RMT_ERROR:"); \
-             printf(__VA_ARGS__);  \
+             fprintf(stderr, "RMT_ERROR:"); \
+             fprintf(stderr, __VA_ARGS__);  \
           } while(0);
 #else
  # define RMT_ERROR
